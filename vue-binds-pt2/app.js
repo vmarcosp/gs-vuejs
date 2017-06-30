c282865f55e5651fd31bd72e0a6f0f93e27c0519
vmarcosp <marcosvcontato.mv@gmail.com>
2017-06-30 01:37:42 -0300

    Criada pasta da segunda parte de binds do vuejs

---
+++ vue-binds-pt2/app.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        message:'V-Bind :D or V-Once',
+        pureHtml:'<strong>Is process html!</strong>',
+        onlyText:'<strong>Is not process html!!</strong>'
+    }
+})
